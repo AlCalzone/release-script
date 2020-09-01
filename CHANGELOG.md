f48001cbf9a6f446067b500c08d2fa9460dd7da6
Dominic Griesel <d.griesel@gmx.net>
2020-09-01 13:36:45 +0200

    fix: use machine-readable replacements for git status

---
+++ CHANGELOG.md
@@ -0,0 +1,18 @@
+# Changelog
+<!--
+    Placeholder for the next version (at the beginning of the line):
+    ## __WORK IN PROGRESS__
+-->
+
+## __WORK IN PROGRESS__
+* Fix: The git branch status is now detected using machine-readable commands instead of the (possibly localized) `git status`
+
+## v1.7.0 (2020-08-24)
+* It is now possible to configure some of the settings with a config file
+* Added the `beforePush` hook to run scripts before creating and pushing the release commit
+
+## v1.6.0 (2020-06-21)
+* Added support for monorepos that are managed with [lerna](https://github.com/lerna/lerna)
+
+## v1.5.1 (2020-06-06)
+* Added support for splitting the changelog into README.md and CHANGELOG_OLD.md
---
+++ src/gitStatus.ts
@@ -0,0 +1,111 @@
+import { execSync, ExecOptions } from "child_process";
+
+type GitStatus = "diverged" | "uncommitted" | "behind" | "ahead" | "up-to-date";
+
+function getExecOptions(cwd: string): ExecOptions & { encoding: "utf8" } {
+	return { cwd, encoding: "utf8" };
+}
+
+function getUpstream(cwd: string): string {
+	return execSync(
+		"git rev-parse --abbrev-ref --symbolic-full-name @{u}",
+		getExecOptions(cwd),
+	).trim();
+}
+
+function getCommitDifferences(cwd: string): [local: number, remote: number] {
+	const output = execSync(
+		`git rev-list --left-right --count HEAD...${getUpstream(cwd)}`,
+		getExecOptions(cwd),
+	).trim();
+	// something like "1\t0"
+	return output.split("\t", 2).map(Number) as any;
+}
+
+function hasUncommittedChanges(cwd: string): boolean {
+	const output = execSync(
+		`git status --porcelain`,
+		getExecOptions(cwd),
+	).trim();
+	return output !== "";
+}
+
+/** Locale-independent check for uncommitted changes and commit differences between local and remote branches */
+export function gitStatus(cwd: string): GitStatus {
+	const [local, remote] = getCommitDifferences(cwd);
+	if (local > 0 && remote > 0) {
+		return "diverged";
+	} else if (local === 0 && remote > 0) {
+		return "behind";
+	} else if (hasUncommittedChanges(cwd)) {
+		return "uncommitted";
+	} /* if (remote === 0) */ else {
+		return local === 0 ? "up-to-date" : "ahead";
+	}
+}
+
+// // check if there are untracked changes
+// const gitStatus = execSync("git status", { cwd: rootDir, encoding: "utf8" });
+// if (/have diverged/.test(gitStatus)) {
+// 	if (!isDryRun) {
+// 		fail(
+// 			colors.red(
+// 				"Cannot continue, the local branch has diverged from the git repo!",
+// 			),
+// 		);
+// 	} else {
+// 		console.log(
+// 			colors.red(
+// 				"This is a dry run. The full run would fail due to a diverged branch\n",
+// 			),
+// 		);
+// 	}
+// } else if (!lerna && !/working tree clean/.test(gitStatus)) {
+// 	if (!isDryRun && !allChanges) {
+// 		fail(
+// 			colors.red(
+// 				`Cannot continue, the local branch has uncommitted changes! Add them to a separate commit first or add the "--all" option to include them in the release commit.`,
+// 			),
+// 		);
+// 	} else {
+// 		if (allChanges) {
+// 			console.warn(
+// 				colors.yellow(
+// 					`Your branch has uncommitted changes that will be included in the release commit!
+// Consider adding them to a separate commit first.
+// `,
+// 				),
+// 			);
+// 		} else {
+// 			console.log(
+// 				colors.red(
+// 					`This is a dry run. The full run would fail due to uncommitted changes.
+// Add them to a separate commit first or add the "--all" option to include them in the release commit.
+// `,
+// 				),
+// 			);
+// 		}
+// 	}
+// } else if (/Your branch is behind/.test(gitStatus)) {
+// 	if (!isDryRun) {
+// 		fail(
+// 			colors.red(
+// 				"Cannot continue, the local branch is behind the remote changes!",
+// 			),
+// 		);
+// 	} else {
+// 		console.log(
+// 			colors.red(
+// 				"This is a dry run. The full run would fail due to the local branch being behind\n",
+// 			),
+// 		);
+// 	}
+// } else if (
+// 	/Your branch is up\-to\-date/.test(gitStatus) ||
+// 	/Your branch is ahead/.test(gitStatus)
+// ) {
+// 	// all good
+// 	if (!lerna) {
+// 		console.log(colors.green("git status is good - I can continue..."));
+// 	}
+// }
